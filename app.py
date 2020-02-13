48b8ca905495b649fed26aa8f6abbefb5e3340d3
shaul.rosner shaul.rosner <shaul.rosner@gmail.com>
2020-02-13 22:52:37 +0200

    added fully random with iterations

--- app.py
+++ app.py
@@ -27,8 +27,19 @@ def main():
     pizzas = []
     for i, pizza in enumerate(pizza_sizes):
         pizzas.append(Pizza(i, pizza))
-    chosen_indices = fully_randomized(slices_to_order, pizzas.copy())
+    #chosen_indices, current_size = fully_randomized(slices_to_order, pizzas.copy())
+    chosen_indices, current_size = retrying_randomized(slices_to_order, pizzas.copy())
     write_output('./outputs' + data + '.out', chosen_indices)
+
+def retrying_randomized(slices_to_order, pizzas, iterations = 1000):
+    best_size = 0
+    best_indices = []
+    for _ in range(iterations):
+        chosen_indices, current_size = fully_randomized(slices_to_order, pizzas.copy())
+        if current_size > best_size:
+            best_size = current_size
+            best_indices = chosen_indices
+    return best_indices, best_size
 
 
 def fully_randomized(slices_to_order, pizzas):
@@ -47,7 +58,7 @@ def fully_randomized(slices_to_order, pizzas):
         if current_size + pizza.size <= slices_to_order:
             current_size += pizza.size
             chosen_indices.append(pizza.index)
-    return chosen_indices
+    return chosen_indices, current_size
 
 
 def write_output(output_name, pizzas):
@@ -57,4 +68,7 @@ def write_output(output_name, pizzas):
 
 
 if __name__ == '__main__':
-    main()
+    for file in ALL_FILES:
+        OPENED_FILE = file
+        main()
+    #main()
