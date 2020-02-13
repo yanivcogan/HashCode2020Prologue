8a055a64e04b78b649b1190bad6e7d90aeef6610
itamar <itamar@feezback.com>
2020-02-13 22:02:02 +0200

    let's make it beautiful

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import numpy as np
 
-if __name__ == '__main__':
-    data = './e_also_big.in'
-    with open(data, 'r') as f:
-        lines = f.readlines()
+# OPENED_FILE = "./e_also_big.in"
+OPENED_FILE = "./b_small.in"
+
+
+def main():
+    data = OPENED_FILE
+    with open(data) as file:
+        lines = file.readlines()
     first_line_parts = lines[0].split(' ')
     slices_to_order = int(first_line_parts[0])
     pizza_types = int(first_line_parts[1])
     pizza_sizes = np.array(lines[1].split(' '), dtype=int)
     print(pizza_sizes)
 
+
 def write_output(output_name, pizzas):
     with open(output_name, 'w') as f:
         f.write(len(pizzas))
-        f.write(str.join(' ', pizzes))
+        f.write(str.join(' ', pizzas))
+
+
+if __name__ == '__main__':
+    main()
